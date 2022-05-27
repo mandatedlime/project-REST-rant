7b7dce5a0a476a0395ece812af5e9475e266e880
Joram Obbus <joramobbus@gmail.com>
2022-05-26 19:30:17 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT;
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 app.use('/places', require('./controllers/places'))
