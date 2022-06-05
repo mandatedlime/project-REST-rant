af002ddeea19da2ceedfe32001914b5b103cb813
Joram Obbus <joramobbus@gmail.com>
2022-06-05 16:55:53 -0700

    readme updated

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const PORT = process.env.PORT;
 const methodOverride = require('method-override')
 
 
+// Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({ extended: true }))
-app.use('/places', require('./controllers/places'))
 
-app.use(methodOverride('_method'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home');
