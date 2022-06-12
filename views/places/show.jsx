7bed4c22c3d16d324d7b2cd067fa72ca2b953b4f
Joram Obbus <joramobbus@gmail.com>
2022-06-12 12:26:17 -0700

    part 9

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,16 +8,46 @@ function show (data) {
             <div className='row'>
               <div className='col-sm-6'>
                 <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
               </div>
-            </div>
             
-            <h2>Description</h2>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                  <button type="submit" className="btn btn-danger">
-                           Delete
-                  </button>
-            </form>   
+             <div className='col-sm-6'>
+               <h1>{ data.place.name}</h1>
+               <h2>
+                 Rating
+               </h2>
+               <h3 className='inactive'>
+                 Not Rated
+               </h3>
+               <br></br>
+               <h2>
+                 Description
+               </h2>
+               <h3>
+                 Located in {data.place.city}, {data.place.state}
+               </h3>
+               <h3>
+                 Serving {data.place.cuisines}
+               </h3>
+               <br />
+               <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                 edit
+               </a>{` `}
+               <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                 <button type="submit" className="btn btn-danger">
+                   Delete
+                 </button>
+               </form>
+               <hr />
+               <h2>Comments</h2>
+               <h3 className='inactive'>
+                 No comments yet!
+               </h3>
+              </div> 
+            </div>  
+             
           </main>
         </Def>
     )
