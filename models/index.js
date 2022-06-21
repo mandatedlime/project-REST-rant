9a4bbee1185f790769c9cf2317f1909deee56cfd
Joram Obbus <joramobbus@gmail.com>
2022-06-20 20:22:16 -0700

    updates

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://joramobbus:<password>@cluster0.ao1j6jd.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://joramobbus:jo5115621@cluster0.ao1j6jd.mongodb.net/rest-rant', {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
